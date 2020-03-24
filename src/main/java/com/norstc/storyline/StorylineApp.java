a13d2c445225b332cdcbcb24be1db7aefac58001
norstc <norstc@gmail.com>
2020-03-24 18:38:15 +0800

    initial version
    create project

---
+++ src/main/java/com/norstc/storyline/StorylineApp.java
@@ -0,0 +1,13 @@
+package com.norstc.storyline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorylineApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorylineApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/norstc/storyline/StroylineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.norstc.storyline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StroylineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
